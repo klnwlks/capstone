cbdd07ce275ad918b7be27b8b4b6c747ca592621
klnwlks <kalinawlukasaombebis@gmail.com>
2025-01-25 19:01:08 +0800

    finished simulation for gear ratios

---
+++ vawtgearratio.py
@@ -0,0 +1,43 @@
+import math
+
+GENERATOREFF = 0.85 # find source for this number
+GENERATORRATEDSPEED = 1500 # IDEAL RPM
+TSR = 2
+BETZ = 0.35
+DENSITY = 1.225 # METER
+TRADIUS = 1.64 # METER
+HEIGHT = 5.17 # METER
+
+def gearratio(WINDSPEED):
+  print(f"DIAGNOSTICS AT AVERAGE WINDSPEED {WINDSPEED}M/S")
+  LOWEST = WINDSPEED - 2
+  HIGHEST = WINDSPEED + 20 # arbitrary values but for goodness sake
+
+  print('------------------------------------------')
+  print(f"LOWEST WIND SPEED: {LOWEST} \nHIGHEST: {HIGHEST}")
+
+  # conv to rads 
+  RPMRAD = (GENERATORRATEDSPEED * 2 * math.pi) / 60
+  LOWESTROTATIONALSPEED = (TSR * LOWEST) / TRADIUS
+  HIGHESTROTATIONALSPEED = (TSR * LOWEST) / TRADIUS
+  RATEDROTATIONALSPEED = (TSR * WINDSPEED) / TRADIUS
+
+  # wind turbine torque is mechanical power / rotational speed of turbine
+  MECHPOWER = 0.5 * DENSITY * (2 * TRADIUS * HEIGHT) * WINDSPEED**3 * BETZ
+  TORQUE = MECHPOWER / RATEDROTATIONALSPEED
+
+  # formula for gear ratio = GENSPEE / ROTATIONAL SPEED OF TURBINE
+  HIGHESTGR = RPMRAD / HIGHESTROTATIONALSPEED
+  LOWESTGR = RPMRAD / LOWESTROTATIONALSPEED
+  AVERAGEGR = RPMRAD / RATEDROTATIONALSPEED
+  print('------------------------------------------')
+  print(f"UPPER BOUND: {HIGHESTGR} \nLOWEST: {LOWESTGR} \nAVERAGE: {AVERAGEGR}")
+
+  # torque for G is Tturbine * GENEFF * GEAR RATIO 
+  GENTORQUE = TORQUE * GENERATOREFF * AVERAGEGR
+  print('------------------------------------------')
+  print(f"TURBINE TORQUE: {TORQUE} \nGENERATOR TORQUE: {GENTORQUE} ")
+
+
+wind = int(input("AVERAGE WINDSPEED OF AREA: "))
+gearratio(wind)
